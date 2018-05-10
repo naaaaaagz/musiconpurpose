ca6e379ca2a7ae1eac03fd75bb4f98e4aec9920a
Patricia Blesa <blesapati@gmail.com>
2018-05-10 20:11:10 +0200

    add contact menu item to the header (#3)

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 export default class Header extends React.Component {
-  
+
   render() {
     return (
       <div className="header">
-        <h1>// Jean Nine</h1>
+        <h1>///nagz////////</h1>
+        <a href="#">contact</a>
       </div>
     );
   }
-  
+
 }
