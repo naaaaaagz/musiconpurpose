82fd7fa20dbd5d2bdb222f322fc7adb0f807df47
Patricia Blesa <blesapati@gmail.com>
2018-06-08 19:03:09 +0200

    create footer component and add basic style (#6)
    * create footer component and add basic style
    
    * fix // comment issue

---
+++ client/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+require('style/footer.styl');
+
+export default class Footer extends React.Component {
+
+  render() {
+    return (
+      <div className="footer">
+        <p>site by <a href="https://github.com/blepati">blepati</a> & <a href="https://github.com/hubudibu">hubudibu </a>& vlad, thx to jean9 &#47; &#47; &#169;  2018</p>
+      </div>
+    );
+  }
+
+}
