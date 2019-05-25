6803610ab89ae5b9440d8a81c71e23206c0b0788
Blanka Hooz <hooz.blanka@gmail.com>
2019-05-25 21:07:33 +0200

    fix: modal does not make the background disappear anymore

--- client/components/ContactModal.js
+++ client/components/ContactModal.js
@@ -1,30 +1,34 @@
 import React from 'react';
-import Modal from 'react-responsive-modal';
+require('../style/contactmodal.styl');
 
 export default class ContactModal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      open: false,
-    }
-  };
+      open: false
+    };
+  }
 
   onOpenModal() {
     this.setState({ open: true });
-  };
+  }
 
   onCloseModal() {
     this.setState({ open: false });
-  };
+  }
 
   render() {
     const { open } = this.state;
     return (
-      <div className="contactButton">
+      <div className='contactButton'>
         <button onClick={this.onOpenModal.bind(this)}>Contact</button>
-        <Modal open={open} onClose={this.onCloseModal.bind(this)} center>
-          <h2>Simple centered modal</h2>
-        </Modal>
+        <div className={open ? 'modalOpen' : 'modalClosed'}>
+          <div className='contactModal'>
+            <button className="contactModalCloseButton" onClick={this.onCloseModal.bind(this)}>x</button>
+            <div className="ContactModalContent">
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
