d0251848502be2c4440ba0e8363439aa1465bcb4
Gyorgy Pakozdi <gyorgy.pakozdi@sigmatechnology.se>
2019-05-26 14:10:18 +0200

    fix(header): layout

--- client/components/Header.js
+++ client/components/Header.js
@@ -7,8 +7,10 @@ export default class Header extends React.Component {
     return (
       <div className="header">
         <h1>///nagz////////</h1>
-        <ContactModal />
-        <a href="http://fanlink.to/nagz" target="_blank"><button>buy&#47;stream</button></a>
+        <div className="buttonContainer">
+          <ContactModal />
+          <a href="http://fanlink.to/nagz" target="_blank"><button>buy&#47;stream</button></a>
+        </div>
       </div>
     );
   }
