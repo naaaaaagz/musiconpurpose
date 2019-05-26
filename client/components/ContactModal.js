fdf231c2ebdebd8d25163a448c070b7336393c01
Gyorgy Pakozdi <gyorgy.pakozdi@sigmatechnology.se>
2019-05-26 15:13:14 +0200

    feat(contact): it works

--- client/components/ContactModal.js
+++ client/components/ContactModal.js
@@ -5,7 +5,8 @@ export default class ContactModal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      open: false
+      open: false,
+      isSent: false
     };
   }
 
@@ -17,8 +18,18 @@ export default class ContactModal extends React.Component {
     this.setState({ open: false });
   }
 
+  onSubmit(e) {
+    e.preventDefault();
+    this.setState({isSent: true});
+    const from_email = e.target.querySelector('input').value;
+    const message = e.target.querySelector('textarea').value;
+    var service_id = "default_service";
+    var template_id = "template_sgmZHsMh";
+    emailjs.send(service_id, template_id, {from_email, message});
+  }
+
   render() {
-    const { open } = this.state;
+    const { open, isSent } = this.state;
     return (
       <div className='contactButton'>
         <button onClick={this.onOpenModal.bind(this)}>Contact</button>
@@ -26,6 +37,15 @@ export default class ContactModal extends React.Component {
           <div className='contactModal'>
             <button className="contactModalCloseButton" onClick={this.onCloseModal.bind(this)}>x</button>
             <div className="ContactModalContent">
+              {isSent ? <h2>Message sent successfully</h2> : 
+                <form className="contactForm" onSubmit={this.onSubmit.bind(this)}>
+                  <label for="email">E-mail:</label>
+                  <input id="email" className="contactInput" type="email" />
+                  <label for="message">Message:</label>
+                  <textarea id="message" className="contactInput"></textarea>
+                  <input className="submitButton" type="submit"  />
+                </form>
+              }
             </div>
           </div>
         </div>
