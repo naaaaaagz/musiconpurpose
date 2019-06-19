238408774c630d7d0a6d25a886916dc22d363882
Gyorgy Pakozdi <gyorgy.pakozdi@sigmatechnology.se>
2019-06-19 22:27:17 +0200

    fix: footer spacing

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -6,7 +6,7 @@ export default class Footer extends React.Component {
   render() {
     return (
       <div className="footer">
-        <p>site by <a href="https://github.com/hubudibu">hubudibu</a>& friends - thx to jean9 &#47;&#47; &#169; 2019</p>
+        <p>site by <a href="https://github.com/hubudibu">hubudibu</a> & friends - thx to jean9 &#47;&#47; &#169; 2019</p>
       </div>
     );
   }
